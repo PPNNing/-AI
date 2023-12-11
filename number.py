11ce5a99eb409333922bb6ce7dfe8a9d876b88b7
Kunming Wang <144293894+KunmW@users.noreply.github.com>
2023-12-11 20:53:07 +0800

    Update number.py

--- number.py
+++ number.py
@@ -4,19 +4,17 @@ import cv2
 import numpy as np
 
 def get_number(img):
-    rawImage = Image.open(img)
-    rawImage=rawImage.crop((1160,1177,1211,1204))
-    rawImage=np.array(rawImage)
-    height, width,deep = rawImage.shape
-    gray = cv2.cvtColor(rawImage, cv2.COLOR_BGR2GRAY)
-    dst = np.zeros((height, width, 1), np.uint8)
-    for i in range(0, height):
-        for j in range(0, width):
-            grayPixel = gray[i, j]
-            dst[i, j] = 255 - grayPixel
-    ret, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    string = pytesseract.image_to_string(binary, lang='eng', config='--psm 6 --oem 3 -c ''tessedit_char_whitelist''=0123456789')
-    return int(string)
-
-img="1.png"#截图路径
-print(get_number(img))
+    img=img.crop((1160,1177,1211,1206))
+    img.save("number.png")
+    img=cv2.imread("number.png")
+    for i in range(0,29):
+        for j in range(0,51):
+            if img[i,j,0]<100:
+                img[i,j]=[255,255,255]
+            else:
+                img[i][j]=[0,0,0]
+    img = cv2.resize(img, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC)
+    gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thr = cv2.threshold(gry, 127, 255, cv2.THRESH_BINARY_INV)[1]
+    res = pytesseract.image_to_string(thr,lang='eng',config=r'-c tessedit_char_whitelist=0123456789 --psm 6')
+    return int(res)
